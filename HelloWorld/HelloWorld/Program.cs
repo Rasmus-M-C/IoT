4b9e530119305ea02f9a007327e9e766e7dd6965
Rasmus-M-C <rasmusmcarlsen@gmail.com>
2024-03-16 02:52:04 +0100

    Add project files.

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+Console.Write("Hello world!");
---
+++ LogWeatherData/LogWeatherData/Program.cs
@@ -0,0 +1,19 @@
+﻿using Iot.Device.Common;
+using Iot.Device.SenseHat;
+using (var th = new SenseHatTemperatureAndHumidity())
+{
+    while (true)
+    {
+        var tempValue = th.Temperature;
+        var humValue = th.Humidity;
+
+        Console.WriteLine($"Temperature: {tempValue:0.#}\u00B0C");
+        Console.WriteLine($"Relative humidity: {humValue:0.#}%");
+
+        // WeatherHelper supports more calculations, such as saturated vapor pressure, actual vapor pressure and absolute humidity.
+        Console.WriteLine($"Heat index: {WeatherHelper.CalculateHeatIndex(tempValue, humValue):0.#}\u00B0C");
+        Console.WriteLine($"Dew point: {WeatherHelper.CalculateDewPoint(tempValue, humValue):0.#}\u00B0C");
+
+        Thread.Sleep(1000);
+    }
+}
